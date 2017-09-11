4a5acd6385920e4069e309ed08ffb2a7bcb8335e
FreeFlight-Code <davidfisc@hotmail.com>
2017-09-11 15:15:31 -0600

    jobs sorting done

--- src/components/Form.js
+++ src/components/Form.js
@@ -20,73 +20,59 @@ export default class Form extends Component {
 
   }
   // handleSearch() {
-    // console.log('starting handlesearch')
-    // var params = undefined;
-    // if (this.state.name) {
-    //   params="?name=" + this.state.name.toString();
-    // }
-    // axios.get('http://localhost:3030/api/jobs' + (params?params:'')).then((res) => {
-      
+  // console.log('starting handlesearch')
+  // var params = undefined;
+  // if (this.state.name) {
+  //   params="?name=" + this.state.name.toString();
+  // }
+  // axios.get('http://localhost:3030/api/jobs' + (params?params:'')).then((res) => {
+
 
   handleSearch(event) {
     event.preventDefault()
     axios.get(`http://localhost:3030/api/jobs`).then((res) => {
-      console.log(res)
-      
+      // console.log(res.data)
+      let name = (this.state.name).toLowerCase();
+      let state = (this.state.state).toLowerCase();
+      let city = (this.state.city).toLowerCase();
+      // let comments = (this.state.comments).toLowerCase();
+      let sortedData = res.data;
+
+      console.log(sortedData)
+      //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    sort names
+      if (this.state.name) {
+        sortedData = sortedData.filter((e) => {
+          let index = (e.jobname).toLowerCase();
+          console.log(index)
+          return index.includes(name);
+        })
+      }
+      //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    sort state
+      if (this.state.state) {
+        sortedData = sortedData.filter((e) => {
+          let index = (e.state).toLowerCase();
+          // console.log(index)
+          return index.includes(state);
+        })
+      }
+      //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    sort city
+
+      if (this.state.city) {
+        sortedData = sortedData.filter((e) => {
+          let index = (e.city).toLowerCase();
+          // console.log(index)
+          return index.includes(city);
+        })
+      }
+
 
       this.setState({
-        list: res.data
+        list: sortedData
       })
+
+
     })
   }
-
-  // return axios.get(`http://localhost:3030/api/jobs`).then((res) => {
-    
-  //    console.log(res);
-  //    // console.log(name);
-  //    // if (this.state.name) {
-  //    //   sortedData = sortedData.filter((e) => {
-  //    //     let index = (e.jobname).toLowerCase();
-  //    //     console.log(index)
-  //    //     return index.includes(name);
-  //    //   })
-  //    }
-  //    // if (this.state.date) {
-  //    //   sortedData = sortedData.filter((e) => {
-  //    //     let index = (e.date);
-  //    //     // console.log(index)
-  //    //     return index.includes(date);
-  //    //   })
-  //    // }
-  //    // if (this.state.state) {
-  //    //   sortedData = sortedData.filter((e) => {
-  //    //     let index = (e.state).toLowerCase();
-  //    //     // console.log(index)
-  //    //     return index.includes(state);
-  //    //   })
-  //    // }
-  //    // if (this.state.city) {
-  //    //   sortedData = sortedData.filter((e) => {
-  //    //     let index = (e.city).toLowerCase();
-  //    //     // console.log(index)
-  //    //     return index.includes(city);
-  //    //   })
-  //    // }
-  //    // if (this.state.comments) {
-  //    //   sortedData = sortedData.filter((e) => {
-  //    //     let index = (e.comments).toLowerCase();
-  //    //     // console.log(index)
-  //    //     return index.includes(comments);
-  //    //   })
-  //    // }
-
-  //  //   this.setState({
-  //  //     list: sortedData
-  //  //   })
-  //  ).catch(error => {
-  //    console.log(error)
-  //  }) 
-
 
   handleChange(e) {
 
@@ -94,15 +80,13 @@ export default class Form extends Component {
 
   render() {
 
-    // var myList = `{<li>} ${this.state.list[0].jobname} {</li>}`;
-
     return (
       <div className="Form">
         <div className="results">
           <ul className='list_container'>
             {this.state.list.map((element, i) => {
               return (
-                <li key={element.id}> 
+                <li key={element.id}>
                   {element.id}
                   {element.jobname}
                   {element.jobdate}
@@ -116,12 +100,12 @@ export default class Form extends Component {
 
         <form className='form_container' action="">
           Search By:
-          <input onChange={(e) => this.setState ({ 'jobname': e.target.value})} type="text" placeholder='Job name' value={this.state.name}></input>
-          <input onChange={(e) => this.setState ({ 'date': e.target.value})} type="text" placeholder='Date of job' ></input>
-          <input onChange={(e) => this.setState ({ 'state': e.target.value})}
-         type="text" placeholder='State' value={this.state.state}></input>
-          <input onChange={(e) => this.setState ({ 'city': e.target.value})} type="text" placeholder='City' value={this.state.city}></input>
-          <input onChange={(e) => this.setState ({ 'comments': e.target.value})} type="text" placeholder='comments' value={this.state.comments}></input>
+          <input onChange={(e) => this.setState({ 'name': e.target.value })} type="text" placeholder='Job name' value={this.state.name}></input>
+          <input onChange={(e) => this.setState({ 'date': e.target.value })} type="text" placeholder='Date of job' ></input>
+          <input onChange={(e) => this.setState({ 'state': e.target.value })}
+            type="text" placeholder='State' value={this.state.state}></input>
+          <input onChange={(e) => this.setState({ 'city': e.target.value })} type="text" placeholder='City' value={this.state.city}></input>
+          <input onChange={(e) => this.setState({ 'comments': e.target.value })} type="text" placeholder='comments' value={this.state.comments}></input>
 
           <button onClick={this.handleSearch} className="searchButton">Search</button>
         </form>
